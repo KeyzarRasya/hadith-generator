b0ae5c57c66a04496ebe05825f6b8e5fd03a3c22
KeyzarRasya <keyzar.r.athallah@gmail.com>
2024-02-08 20:37:41 +0700

    path

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const { default: axios } = require("axios");
 const express = require("express")
+const path = require("path")
 const app = express();
 
 app.use(express.json())
 app.use(express.static("views"))
 app.set("view engine", "ejs")
+app.set('views', path.join(__dirname, 'views'));
 
 const API = 'https://random-hadith-generator.vercel.app/bukhari/'
 app.get("/", (req, res) => {
