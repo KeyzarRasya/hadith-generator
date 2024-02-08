c1fb90a5033c483dfa5e8b3b2a04ea4abbda34a8
KeyzarRasya <keyzar.r.athallah@gmail.com>
2024-02-08 20:32:56 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const { default: axios } = require("axios");
+const express = require("express")
+const app = express();
+
+app.use(express.json())
+app.use(express.static("views"))
+app.set("view engine", "ejs")
+
+const API = 'https://random-hadith-generator.vercel.app/bukhari/'
+app.get("/", (req, res) => {
+    axios.get(API, {
+        method:"GET",
+        headers:{
+            Accept:"application/json"
+        }
+    }).then(response => {
+        res.render("home", {res:response.data.data})
+    }).catch(err => console.log(err))
+})
+
+app.listen(3000, () => {
+    console.log("On")
+})
